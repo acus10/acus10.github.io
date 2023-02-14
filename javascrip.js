f6986b4c5f0533b77390be3030fa76210e5fc2b5
acus10 <116589084+acus10@users.noreply.github.com>
2023-02-14 09:42:05 -0300

    Update javascrip.js

--- javascrip.js
+++ javascrip.js
@@ -16,3 +16,29 @@ function addLi2(){
     liNode.appendChild(txtNode);
     listNode.appendChild(liNode);                        
 }
+function editCv(){                
+  var x = document.getElementById('txtVal');
+  if (x.style.visibility === 'hidden') {
+    x.style.visibility = 'visible';
+  } else {
+    x.style.visibility = 'hidden';
+  }
+  x = document.getElementById('txtVal2');
+  if (x.style.visibility === 'hidden') {
+    x.style.visibility = 'visible';
+  } else {
+    x.style.visibility = 'hidden';
+  }
+  x = document.getElementById('button-soft-skills');
+  if (x.style.visibility === 'hidden') {
+    x.style.visibility = 'visible';
+  } else {
+    x.style.visibility = 'hidden';
+  }
+  x = document.getElementById('button-language');
+  if (x.style.visibility === 'hidden') {
+    x.style.visibility = 'visible';
+  } else {
+    x.style.visibility = 'hidden';
+  }
+}
