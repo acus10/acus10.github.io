c9a37ab354372d7d4313614c4370c035cdda1fb6
acus10 <116589084+acus10@users.noreply.github.com>
2023-02-14 03:40:21 -0300

    Update javascrip.js

--- javascrip.js
+++ javascrip.js
@@ -1,18 +1,18 @@
-let mainNavLinks = document.querySelectorAll("nav ul li a");
-
-window.addEventListener("scroll", event => {
-    let fromTop = window.scrollY;
-
-    mainNavLinks.forEach(link => {
-    let section = document.querySelector(link.hash);
-
-    if (
-      section.offsetTop <= fromTop &&
-      section.offsetTop + section.offsetHeight > fromTop
-    ) {
-      link.classList.add("current");
-    } else {
-      link.classList.remove("current");
-    }
-  });
-});
+function addLi(){          
+    var txtVal = document.getElementById('txtVal').value,
+        listNode = document.getElementById('inline-language'),
+        liNode = document.createElement("LI"),
+        txtNode = document.createTextNode(txtVal);
+                        
+    liNode.appendChild(txtNode);
+    listNode.appendChild(liNode);                        
+}
+function addLi2(){            
+    var txtVal = document.getElementById('txtVal2').value,
+        listNode = document.getElementById('inline-soft-skills'),
+        liNode = document.createElement("LI"),
+        txtNode = document.createTextNode(txtVal);
+                        
+    liNode.appendChild(txtNode);
+    listNode.appendChild(liNode);                        
+}
