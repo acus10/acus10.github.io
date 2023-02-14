3ce94aed05c7fc6d98d409ec7dc56822b7826ab6
acus10 <116589084+acus10@users.noreply.github.com>
2023-02-14 19:03:26 -0300

    Update javascrip.js

--- javascrip.js
+++ javascrip.js
@@ -41,4 +41,41 @@ function editCv(){
   } else {
     x.style.visibility = 'hidden';
   }
+  x = document.getElementById('button-personal');
+  if (x.style.visibility === 'hidden') {
+    x.style.visibility = 'visible';
+  } else {
+    x.style.visibility = 'hidden';
+  }
+  x = document.getElementById('txt-personal');
+  if (x.style.visibility === 'hidden') {
+    x.style.visibility = 'visible';
+  } else {
+    x.style.visibility = 'hidden';
+  }
+}
+var items = document.querySelectorAll("#personal-info li"),
+    inputText = document.getElementById("txt-personal"),
+    tab = [], liIndex ;             
+    // populate tab with li value
+for(var i = 0; i < items.length; i++){
+  tab.push(items[i].innerHTML);
+}
+// get selected li into text fields and get the index             
+for(var i = 0; i < items.length; i++){
+  items[i].onclick = function(){                     
+                     inputText.value = this.innerHTML;
+                     liIndex = tab.indexOf(this.innerHTML);
+                     console.log(this.innerHTML +" INDEX = " + liIndex);
+                     
+                 };
+}
+function editLI(){
+  items[liIndex].innerHTML = inputText.value;                 
+  // clear array
+  tab.length = 0;                 
+  // fill array
+  for(var i = 0; i < items.length; i++){
+    tab.push(items[i].innerHTML);
+  }
 }
